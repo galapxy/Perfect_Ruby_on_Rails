49f91b83d3eff595712e7ea06c10769e4e73c40e
KeisukeMurata4210 <keisuke.murata4210@gmail.com>
2018-08-11 10:26:55 +0900

    自作バリデーション

--- book_admin/app/models/book.rb
+++ book_admin/app/models/book.rb
@@ -7,4 +7,15 @@ class Book < ActiveRecord::Base
 
     has_many :book_authors
     has_many :authors, through: :book_authors
+
+    # 本の名前は必ず入っていてほしい
+    # 本の名前は、15文字までに収まっていて欲しい
+    # 本の価格は、0以上の値になっていてほしい
+    validates :name, presence: true
+    validates :name, length: { maximum: 15 }
+    validates :price, numericality: { greater_than_or_equal_to: 0 }
+    # 独自のバリデーション
+    validates do |book|
+        if book.name.include?("exercise")
+            book.errors[:name] << "I don't like exercise."
 end
