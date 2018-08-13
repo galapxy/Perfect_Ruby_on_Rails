fae0aaed818e4d8eea5891a2dacada6daeed1529
kmurata <kmurata@samurai-net.co.jp>
2018-08-13 09:59:28 +0900

    8/13

--- book_admin/app/controllers/books_controller.rb
+++ book_admin/app/controllers/books_controller.rb
@@ -1,13 +1,17 @@
 class BooksController < ApplicationController
-    # アクションコールバック onlyとかexceptとかある
-    before_action :set_book, only:[:show, :edit, :update, :destroy]
-    # ブロックも使える before_action do ------ end
-    def show
-        @book = Book.find(params[:id])
-    end
+	# アクションコールバック onlyとかexceptとかある
+  before_action :set_book, only:[:show, :edit, :update, :destroy]
+  # ブロックも使える before_action do ------ end
+  def show
+		@book = Book.find(params[:id])
+		respond_to do |format|
+			format.html
+			format.csv
+		end
+  end
 
-    private
-    def set_book
-        @book = Book.find(params[:id])
-    end
+  private
+  def set_book
+    @book = Book.find(params[:id])
+  end
 end
